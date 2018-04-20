559a8085f8a34e6df0975146e81a52f10574e166
Ifat <iribon9@gmail.com>
2018-04-20 14:02:10 -0500

    serializer

--- app/serializers/day_running_exercise_serializer.rb
+++ app/serializers/day_running_exercise_serializer.rb
@@ -1,14 +1,7 @@
 class DayRunningExerciseSerializer < ActiveModel::Serializer
-  # embed :ids
-
   attributes  :id,
               :completed,
               :runnable
 
-  belongs_to :day
   belongs_to :runnable, polymorphic: true
-
-  # def runnable
-  #   object.runnable
-  # end
 end
